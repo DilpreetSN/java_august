82798d03128cec9e6d6638d7f883eaccaef259fa
DilpreetSN <dilpreetpun@gmail.com>
2023-09-16 20:31:27 -0400

    Dilpreet

---
+++ src/SelfPractice/ConditionalStatement1.java
@@ -0,0 +1,22 @@
+package SelfPractice;
+
+import java.util.Scanner;
+
+public class ConditionalStatement1
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Input the 1st number");
+        double num1 = in.nextDouble();
+
+        System.out.println("Input the 2nd number");
+        double num2 = in.nextDouble();
+
+        System.out.println("Input the 3rd number");
+        double num3 = in.nextDouble();
+
+       // if (num)
+    }
+}
---
+++ src/SelfPractice/Practice1ifelse.java
@@ -0,0 +1,24 @@
+package SelfPractice;
+
+import java.util.Scanner;
+
+public class Practice1ifelse {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the input: ");
+        int input = in.nextInt();
+        if(input>0)
+        {
+            System.out.println("number is postive");
+        }
+        else if (input<0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+    }
+}
